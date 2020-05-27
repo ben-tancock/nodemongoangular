82ab977b57d84fab3a7cfe899d2ffcb5d92403bc
ben-tancock <ben.tancock@gmail.com>
2020-05-26 20:24:33 -0400

    installed express, did express routing

---
+++ express_routes.js
@@ -0,0 +1,35 @@
+// to use this: just type 'localhost/<thing>' in the URL, where thing corresponds to any of the app.get parameters
+
+var express = require('express');
+var url = require('url');
+var app = express();
+app.listen(80);
+
+app.get('/', function(req, res){
+    res.send("Get Index");
+});
+
+app.get('/find', function(req, res){
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    var response = 'Finding book: Author ' + query.author + ' Title: ' + query.title;
+    console.log('\nQuery URL: ' + req.originalUrl);
+    console.log(response);
+    res.send(response);
+});
+
+app.get(/^\/book\/(\w+)\:(\w+)?$/, function(req, res){
+    var response = 'Get Book: Chapter: ' + req.params[0] + ' Page: ' + req.params[1];
+    console.log('\nRegex URL: ' + req.originalUrl);
+    console.log(response);
+    res.send(response);
+});
+
+app.get('/user/:userid', function(req, res){
+    var response = 'Get User: ' + req.param('userID');
+    console.log('\nParam URL: ' + req.originalUrl);
+    console.log(response);
+    res.send(response);
+});
+
+
